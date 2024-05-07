180e64228ced24d47f70c2ca77d5c2639eebf290
dickyanthony <dicky.a@weefer.com>
2024-05-07 23:58:30 +0700

    DA-feat: fix responsive

--- src/components/WrapCenter.jsx
+++ src/components/WrapCenter.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 const WrapCenter = ({ children }) => {
+  const medium = useMediaQuery({ maxWidth: "671px" });
   return (
-    <div className="flex justify-center items-center h-full w-full">
+    <div
+      className="flex justify-center items-center h-full w-full"
+      style={
+        medium
+          ? {
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+            }
+          : {
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+            }
+      }
+    >
       {children}
     </div>
   );
