653f6d6de483852bfa62795ff88ad0fd2e484f52
dickyanthony <dicky.a@weefer.com>
2024-05-10 00:35:17 +0700

    DA-refactor: clean code

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
-import { AuthProvider } from "./hook/auth/Auth.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider className="h-full">
-      {/* <AuthProvider> */}
       <App />
-      {/* </AuthProvider> */}
     </NextUIProvider>
   </React.StrictMode>
 );
