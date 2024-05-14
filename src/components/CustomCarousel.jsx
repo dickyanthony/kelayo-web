b201199f86e1f8fc156339525fe2883267bb84d2
dickyanthony <dickyanthony0812@gmail.com>
2024-05-14 20:03:35 +0700

    DA-feat: add carousel

--- src/components/CustomCarousel.jsx
+++ src/components/CustomCarousel.jsx
@@ -1,13 +1,95 @@
-import Carousel from 'react-carousel';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { WrapHCenter } from ".";
 
 export default function CustomCarousel(props) {
-  const { listImage = [], title, onChange = () => {}, onClickItem = () => {} } = props;
-  <Carousel onChange={onChange} onClickItem={onClickItem} showArrows={false} showThumbs={false}>
-    {listImage.map((x) => (
-      <div>
-        <img src={x} />
-        <p className="legend">{title}</p>
+  const { listImage = [], title = "" } = props;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    appendDots: (dots) => (
+      <div
+        style={{
+          bottom: "10px",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <ul style={{ margin: "0", width: "100%" }}> {dots} </ul>
       </div>
-    ))}
-  </Carousel>;
+    ),
+    customPaging: () => (
+      <button
+        style={{
+          width: "20px",
+          height: "5px",
+          margin: "0 5px",
+          borderRadius: "5px",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+        }}
+      ></button>
+    ),
+  };
+
+  return (
+    <div
+      style={{
+        minWidth: "99.1vw",
+        width: "100%",
+        maxHeight: "calc(100vh - 200px)",
+      }}
+    >
+      <Slider
+        style={{
+          minWidth: "99.1vw",
+          width: "100%",
+          maxHeight: "calc(100vh - 200px)",
+          overflow: "hidden",
+        }}
+        {...settings}
+      >
+        {listImage.map((slide, index) => (
+          <div key={index} style={{ position: "relative" }}>
+            <img
+              src={slide}
+              alt={index}
+              style={{ minWidth: "99.1vw", maxHeight: "calc(100vh - 200px)" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0, 0, 0, 0.05)", // Dark overlay with transparency
+              }}
+            ></div>
+          </div>
+        ))}
+      </Slider>
+
+      <div
+        className="font-bold container max-w-screen-xl text-xl top-[10%] min-[324px]:top-[15%] md:top-[30%] lg:text-5xl lg:top-[35%]"
+        style={{
+          position: "absolute",
+
+          left: "5%",
+          width: "90%",
+          color: "white",
+          textAlign: "left",
+          padding: "10px",
+        }}
+      >
+        {title}
+      </div>
+    </div>
+  );
 }
