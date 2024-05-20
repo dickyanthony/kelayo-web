30d0059d0dd72f7173c438ff518199e1a2d56882
dicky <dicky.a@weefer.co.id>
2024-05-20 09:18:58 +0700

    DA-fix: fix typo

--- src/components/TabRentTransportation.jsx
+++ src/components/TabRentTransportation.jsx
@@ -1,51 +1,51 @@
-import React from "react";
-import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
-import CustomPagination from "./CustomPagination";
-import { ItemRentTransportation } from ".";
-import Fortuner from "../assets/fortuner.png";
-import Beat from "../assets/beat.png";
+import React from 'react';
+import { Tabs, Tab, Card, CardBody } from '@nextui-org/react';
+import CustomPagination from './CustomPagination';
+import { ItemRentTransportation } from '.';
+import Fortuner from '../assets/fortuner.png';
+import Beat from '../assets/beat.png';
 const DATA_CAR = [
   {
     id: 1,
-    name: "Rentcar jogja",
+    name: 'Rentcar jogja',
     image: Fortuner,
   },
   {
     id: 2,
-    name: "Yogya Rental Mobil",
+    name: 'Yogya Rental Mobil',
     image: Fortuner,
   },
   {
     id: 3,
-    name: "jogja Transport",
+    name: 'jogja Transport',
     image: Fortuner,
   },
   {
     id: 4,
-    name: "Jhony Fonsen",
+    name: 'Jhony Fonsen',
     image: Fortuner,
   },
 ];
 const DATA_MOTOR = [
   {
     id: 1,
-    name: "Mandiri Motor",
+    name: 'Mandiri Motor',
     image: Beat,
   },
   {
     id: 2,
-    name: "WS Rental",
+    name: 'WS Rental',
     image: Beat,
   },
   {
     id: 3,
-    name: "Perdana rent",
+    name: 'Perdana rent',
     image: Beat,
   },
 ];
 export default function TabRentTransportation(props) {
   const { setSelectedTab } = props;
-  const [selected, setSelected] = React.useState("photos");
+  const [selected, setSelected] = React.useState('photos');
 
   return (
     <div className="flex w-full flex-col mt-16">
@@ -57,7 +57,7 @@ export default function TabRentTransportation(props) {
           setSelectedTab(e);
         }}
       >
-        <Tab key="car-rental" title="Rentar Mobil">
+        <Tab key="car-rental" title="Rental Mobil">
           <Card>
             <CardBody className="flex items-center justify-center">
               <div
